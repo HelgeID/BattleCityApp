20ebfd47d16005ee79f4b92946612050de590047
HelgeID <helgeguley@gmail.com>
2018-11-07 00:00:00 +0300

    build02(test xml)

--- BattleCityApp/BattleCityApp/source.cpp
+++ BattleCityApp/BattleCityApp/source.cpp
@@ -1,8 +1,16 @@
 ﻿#include <iostream>
+#include <string>
+#include <pugixml.hpp>
 using namespace std;
 
 int main()
 {
+	pugi::xml_document doc;
+	doc.load_file("ui_sprite.xml");
 
+	int x = doc.child("sprite").child("element1").attribute("x").as_int();
+	cerr << x << endl;
+
+	system("pause");
 	return 0;
 }
